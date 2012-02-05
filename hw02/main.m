f0704c57ea17971ff56cc059fd223454ce8cc502
Christopher Brown <io@henrian.com>
2012-02-04 18:03:13 -0600

    Created command line-callable eval function

--- hw02/main.m
+++ hw02/main.m
@@ -6,7 +6,6 @@
 % show(images, 1046);
 
 % turnin to hychyc07
-load hw2Data.mat;
 %nImages = 2500; % = k
 %fprintf('Training on %d samples.\n', nImages);
 % size(trainImages) => 28          28           1       60000
@@ -14,38 +13,15 @@ load hw2Data.mat;
 %[images, labels] = flatten4D(trainImages, trainLabels, 1:nImages);
 % [m, V] = hw2FindEigendigits(images');
 
+nTest = 10000;
+eval(nTest);
+
+
+
 
 % power(2, 1:15)
-nImagesCandidates = [1 2 5 10 25 50 75 100 250 500 1000 5000 10000 30000 50000];
-topNCandidates = [1 2 5 10 25 50 75 100 250 500 750 1000];
 
 % [testImages, testLabels] = flatten4D(trainImages, trainLabels, 50000:49999 + nTest);
-nTest = 10000;
-[testImages, testLabels] = flatten4D(testImages, testLabels, 1:nTest);
-        
-results = zeros(length(nImagesCandidates), length(topNCandidates));
-for ii=1:length(nImagesCandidates)
-    nImages = nImagesCandidates(ii);
-    [images, labels] = flatten4D(trainImages, trainLabels, 1:nImages);
-    [m, fullV] = hw2FindEigendigits(images');
-    for jj=1:length(topNCandidates)
-        topN = topNCandidates(jj);
-        V = fullV(:,1:min(topN, size(fullV, 2)));
-
-        eigenTrain = bsxfun(@minus, images, m') * V;
-        eigenTest = bsxfun(@minus, testImages, m') * V;
-        successes = zeros(1, nTest);
-        for tt=1:nTest
-            testImage = eigenTest(tt,:);
-            goldLabel = testLabels(tt);
-            [label] = knnDistance(testImage, eigenTrain, labels, 10);
-            successes(tt) = label == goldLabel;
-        end
-        result = sum(successes) / length(successes);
-        results(ii, jj) = result;
-        fprintf('got results for nImages = %d, topN = %4d : %0.4f\n', nImages, topN, result);
-    end
-end
 
 % imshow(results);
 % colormap(gray);
